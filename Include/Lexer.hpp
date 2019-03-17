dd4ed19fba74fd7f58d22cc9d8cafab2d13fb44d
Denis <kryden1987@gmail.com>
2019-03-17 18:29:42 +0300

    Minor fix

---
+++ Include/Lexer.hpp
@@ -0,0 +1,25 @@
+#ifndef LEXER_HPP
+#define LEXER_HPP
+
+
+namespace cli
+  {
+
+  class Lexer
+    {
+    public:
+
+
+    private:
+
+
+    };
+
+
+
+  } // end namespace cli
+
+
+
+
+#endif // LEXER_HPP
---
+++ Include/Parser.hpp
@@ -0,0 +1,20 @@
+#ifndef PARSER_HPP
+#define PARSER_HPP
+
+
+namespace cli
+  {
+
+  class Parser
+    {
+    public:
+
+
+    private:
+
+    };
+
+
+  } // end namespace cli
+
+#endif // PARSER_HPP
